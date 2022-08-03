651fb6fe4942d916dd38351f90b90431c6056f61
AdriaGual <u1933571@campus.udg.edu>
2022-08-03 19:11:37 +0200

    changed favicon and app name

--- src/api/lists.tsx
+++ src/api/lists.tsx
@@ -5,7 +5,7 @@ const API_URL = "http://127.0.0.1:8000/api";
 export const createDefaultList = async (userId) => {
   await axios.post(`${API_URL}/lists`, {
     user_id: userId,
-    name: "List",
+    name: "Shopping list",
     active: true,
   });
   return 200;
