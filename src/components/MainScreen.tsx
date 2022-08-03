b1074b20486f50ec78e45d8c4854848950c9a0c2
AdriaGual <u1933571@campus.udg.edu>
2022-08-03 14:37:36 +0200

    added isLoading and error pages

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -3,6 +3,7 @@ import LeftPanel from "./LeftPanel";
 import MainTopBar from "./MainTopBar";
 import { useQuery } from "react-query";
 import { getCategoriesWithItems } from "../api/categories";
+import BeatLoader from "react-spinners/BeatLoader";
 
 function MainScreen() {
   const {
@@ -11,14 +12,33 @@ function MainScreen() {
     isLoading,
   } = useQuery(["categories"], getCategoriesWithItems);
 
-  if (!isLoading) {
-    return <div>isLoading</div>;
+  if (isLoading) {
+    return (
+      <div className="flex h-screen justify-center items-center">
+        <div className="space-y-4">
+          <span className="text-2xl font-semibold">
+            Loading, please wait...
+          </span>
+          <div className="flex justify-center w-full">
+            <BeatLoader color="#F9A109" size={40} speedMultiplier={0.5} />
+          </div>
+        </div>
+      </div>
+    );
   }
 
   if (error) {
-    console.log(error);
-    return <div>isError</div>;
+    return (
+      <div className="flex h-screen justify-center items-center">
+        <div className="space-y-4">
+          <span className="text-2xl font-semibold">
+            An error just occurred, please try again :(
+          </span>
+        </div>
+      </div>
+    );
   }
+
   return (
     <div className="flex flex-no-wrap">
       <LeftPanel></LeftPanel>
