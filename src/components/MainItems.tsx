ac2ce612db0610e308558b121d9aceaecb1b6b6c
AdriaGual <u1933571@campus.udg.edu>
2022-08-03 16:46:21 +0200

    improve media queries for items

--- src/components/MainItems.tsx
+++ src/components/MainItems.tsx
@@ -12,26 +12,29 @@ function MainItems() {
     <div className="font-semibold">
       {categories.map((category) => {
         const items: any = category.items;
-        console.log(items.length > 0);
         return items.length > 0 ? (
           <div key={category.id}>
             <div className="text-2xl pb-4">{category.name}</div>
-            <div className="flex space-x-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-6 gap-y-12 mb-16">
               {items.map((item) => {
                 return (
                   <div
                     key={item.id}
-                    className="flex py-4 px-4 bg-white rounded-xl shadow-sm w-52 mb-16"
+                    className="grid grid-cols-6 bg-white rounded-xl shadow-sm"
                   >
-                    <div className="text-xl w-full">{item.name}</div>
-                    <MdOutlineAdd size={28} color="gray"></MdOutlineAdd>
+                    <div className="text-xl p-4 col-span-5">{item.name}</div>
+                    <div className="hover:bg-mainYellow rounded-r-xl">
+                      <div className="py-4 pl-2">
+                        <MdOutlineAdd size={28} color="gray"></MdOutlineAdd>
+                      </div>
+                    </div>
                   </div>
                 );
               })}
             </div>
           </div>
         ) : (
-          <></>
+          <div key={category.id}></div>
         );
       })}
     </div>
--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -43,7 +43,7 @@ function MainScreen() {
   return (
     <div className="flex flex-no-wrap">
       <LeftPanel></LeftPanel>
-      <div className="my-10 mx-20 space-y-12">
+      <div className="my-10 mx-20 space-y-12 w-2/3">
         <MainTopBar></MainTopBar>
         <MainItems></MainItems>
       </div>
