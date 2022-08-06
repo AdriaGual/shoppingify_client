617494efcf568a8edb7b432deddd5b8325eb5c6a
AdriaGual <u1933571@campus.udg.edu>
2022-08-06 11:10:54 +0200

    added references for inputs on create item

--- src/components/MainRightCreateItemPanel.tsx
+++ src/components/MainRightCreateItemPanel.tsx
@@ -1,25 +1,19 @@
-import React from "react";
-import { MdOutlineArrowBack, MdClose } from "react-icons/md";
+import React, { useRef } from "react";
+import { MdOutlineArrowBack } from "react-icons/md";
 import { itemStore } from "../store/ItemStore";
 import { getCategoriesWithItems } from "../api/categories";
 import { useQuery } from "@tanstack/react-query";
 
 function MainRightCreateItemPanel() {
-  const {
-    id,
-    setId,
-    name,
-    setName,
-    category,
-    setCategory,
-    note,
-    setNote,
-    image,
-    setImage,
-    setShowDetails,
-  } = itemStore();
+  const { setId, setName, setCategory, setNote, setImage, setShowDetails } =
+    itemStore();
 
   const { data: categories } = useQuery(["categories"], getCategoriesWithItems);
+
+  const nameInputRef = useRef<HTMLInputElement>(null);
+  const noteInputRef = useRef<HTMLTextAreaElement>(null);
+  const imageInputRef = useRef<HTMLInputElement>(null);
+  const categoryInputRef = useRef<HTMLSelectElement>(null);
 
   function resetItemDetails() {
     setId(0);
@@ -29,6 +23,14 @@ function MainRightCreateItemPanel() {
     setImage("");
     setShowDetails(0);
   }
+
+  function createItem() {
+    console.log(nameInputRef.current?.value);
+    console.log(noteInputRef.current?.value);
+    console.log(imageInputRef.current?.value);
+    console.log(categoryInputRef.current?.value);
+  }
+
   return (
     <div className="w-96 bg-white py-12 px-10 space-y-8">
       <button className="flex space-x-2" onClick={() => resetItemDetails()}>
@@ -39,6 +41,7 @@ function MainRightCreateItemPanel() {
       <div className="space-y-2">
         <div className="text-sm font-bold">Name</div>
         <input
+          ref={nameInputRef}
           placeholder="Enter a name"
           className="shadow-sm rounded-xl p-4 w-full border-2 text-sm border-gray outline-mainYellow"
         />
@@ -46,6 +49,7 @@ function MainRightCreateItemPanel() {
       <div className="space-y-2">
         <div className="text-sm font-bold">Note (optional)</div>
         <textarea
+          ref={noteInputRef}
           placeholder="Enter a note"
           rows={4}
           className="shadow-sm rounded-xl p-4 w-full border-2 text-sm border-gray outline-mainYellow"
@@ -54,6 +58,7 @@ function MainRightCreateItemPanel() {
       <div className="space-y-2">
         <div className="text-sm font-bold">Image (optional)</div>
         <input
+          ref={imageInputRef}
           placeholder="Enter a name"
           className="shadow-sm rounded-xl p-4 w-full border-2 text-sm border-gray outline-mainYellow"
         />
@@ -61,10 +66,12 @@ function MainRightCreateItemPanel() {
       <div className="space-y-2">
         <div className="text-sm font-bold">Category</div>
         <select
+          ref={categoryInputRef}
+          defaultValue={0}
           id="category"
           className="shadow-sm rounded-xl p-4 w-full border-2 text-sm border-gray outline-mainYellow appearance-none"
         >
-          <option selected>Enter a category</option>
+          <option value={0}>Enter a category</option>
           {categories.map((category: any) => {
             return (
               <option key={category.id} value={category.id}>
@@ -80,7 +87,7 @@ function MainRightCreateItemPanel() {
         </button>
         <button
           className="text-white bg-mainYellow py-4 px-6 rounded-xl m-auto"
-          onClick={() => console.log("A")}
+          onClick={() => createItem()}
         >
           Save
         </button>
